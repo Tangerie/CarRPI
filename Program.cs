04bde6c00ed7e28be7051f169d1ee71a714d0f44
Tangerie <josh.cavill01@gmail.com>
2022-10-26 21:50:35 +1100

    faster colors

--- Program.cs
+++ Program.cs
@@ -1,29 +1,44 @@
 ﻿using Raylib_cs;
 
 
-namespace HelloWorld {
-    static class Program {
-        public static void Main() {
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static unsafe void Main()
+        {
             Raylib.InitWindow(1024, 600, "Hello World");
 
-            
+
             double delta = 1.0;
             Color c = new Color();
             c.a = 0xff;
-            while(!Raylib.WindowShouldClose()) {
+            Image im = Raylib.GenImageColor(1024, 600, Color.RED);
+
+            
+
+            for (int x = 0; x < 1024; x++)
+            {
+                c.r = Convert.ToByte((int)(x / 1024.0 * 0xff));
+                for (int y = 0; y < 600; y++)
+                {
+                    c.g = Convert.ToByte((int)((1.0 - y / 600.0) * 0xff));
+                    Raylib.ImageDrawPixel(& im, x, y, c);
+                }
+            }
+
+            Texture2D tex = Raylib.LoadTextureFromImage(im);
+
+            Raylib.UnloadImage(im);
+
+            while (!Raylib.WindowShouldClose())
+            {
                 delta = 1000.0 / Raylib.GetFPS();
                 Raylib.BeginDrawing();
                 // Raylib.ClearBackground(Color.BLACK);
 
-                for(int x = 0; x < 1024; x++) {
-                    c.r = Convert.ToByte((int)(x / 1024.0 * 0xff));
-                    for(int y = 0; y < 600; y++) {
-                        c.g = Convert.ToByte((int)((1.0 - y / 600.0) * 0xff));
-                        Raylib.DrawPixel(x, y, c);
-                    }
-                }
+                Raylib.DrawTexture(tex, 0, 0, Color.WHITE);
 
-                
 
                 Raylib.DrawText("FPS: " + Raylib.GetFPS().ToString(), 10, 10, 20, Color.RED);
                 Raylib.DrawText("D: " + delta.ToString(), 10, 40, 20, Color.RED);

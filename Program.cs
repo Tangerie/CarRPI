d2edfbbd8349d5a1b315185c186c9b0436431a89
Tangerie <josh.cavill01@gmail.com>
2022-10-26 21:39:57 +1100

    colors

--- Program.cs
+++ Program.cs
@@ -6,32 +6,30 @@ namespace HelloWorld {
         public static void Main() {
             Raylib.InitWindow(1024, 600, "Hello World");
 
-            double y = 0.0;
-            double dir = 1.0;
             
             double delta = 1.0;
-
+            Color c = new Color();
+            c.a = 0xff;
             while(!Raylib.WindowShouldClose()) {
                 delta = 1000.0 / Raylib.GetFPS();
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.BLACK);
+                // Raylib.ClearBackground(Color.BLACK);
 
-                Raylib.DrawText("RAYLIB", 500, (int) Math.Round(y), 20, Color.GREEN);
-
-                if(y >= 600) {
-                    dir = -1;
-                } else if (y <= 0) {
-                    dir = 1;
+                for(int x = 0; x < 1024; x++) {
+                    c.r = Convert.ToByte((int)(x / 1024.0 * 0xff));
+                    for(int y = 0; y < 600; y++) {
+                        c.g = Convert.ToByte((int)((1.0 - y / 600.0) * 0xff));
+                        Raylib.DrawPixel(x, y, c);
+                    }
                 }
 
-                if(Raylib.GetFPS() > 10) {
-                    y += dir * delta;
-                }
+                
 
                 Raylib.DrawText("FPS: " + Raylib.GetFPS().ToString(), 10, 10, 20, Color.RED);
                 Raylib.DrawText("D: " + delta.ToString(), 10, 40, 20, Color.RED);
                 Raylib.DrawText("X: " + Raylib.GetTouchX().ToString(), 10, 70, 20, Color.RED);
                 Raylib.DrawText("Y: " + Raylib.GetTouchY().ToString(), 10, 100, 20, Color.RED);
+
 
 
                 Raylib.EndDrawing();

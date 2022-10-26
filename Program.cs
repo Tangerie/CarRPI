87f578a8ecebc1063d002a2aeaea090dccf1aebd
Tangerie <josh.cavill01@gmail.com>
2022-10-26 19:32:16 +1100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Raylib_cs;
+
+
+namespace HelloWorld {
+    static class Program {
+        public static void Main() {
+            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(800, 400, "Hello World");
+
+            while(!Raylib.WindowShouldClose()) {
+                
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
